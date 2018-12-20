ef87979df13bd02e0907d9c9b33f9bb3406038c2
Ridote <daniridao@gmail.com>
2018-12-20 21:23:30 +0000

    Now getting servers with their ports and ips

--- Entities/Interface/StartScreen.gd
+++ Entities/Interface/StartScreen.gd
@@ -14,4 +14,14 @@ func _on_FindGames_pressed():
 
 func _on_retrieveFoundGames(games):
 	for game in games:
-		print(game.name)
+		var button = Button.new()
+		button.text = game.name
+		button.set_name("ButtonServer_" + game.name)
+		button.set_meta("IP", game.ip)
+		button.set_meta("Port", game.port)
+		$HBoxContainer/MenuGames.add_child(button)
+		button.connect("pressed",self,"_on_serverSelected", [button])
+	
+func _on_serverSelected(button):
+	print(button.get_meta("IP"))
+	print(button.get_meta("Port"))

6cc2df91d002c457a79e7d7d023d35b3cd6c4624
Ridote <daniridao@gmail.com>
2018-12-21 18:46:52 +0000

    Start screen wiring up with MP

--- Entities/Interface/StartScreen.gd
+++ Entities/Interface/StartScreen.gd
@@ -3,6 +3,8 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if(MP.connect("broker_list", self, "_on_retrieveFoundGames")):
+		print("ERROR WHEN CONNECTING SIGNALS")
+	if(MP.connect("new_player", self, "_on_newPlayer")):
 		pass
 
 func _on_NewGames_pressed():
@@ -13,15 +15,29 @@ func _on_FindGames_pressed():
 	MP.broker_list()
 
 func _on_retrieveFoundGames(games):
+	if games.size() < 0:
+		return
+	showGames()
 	for game in games:
 		var button = Button.new()
 		button.text = game.name
 		button.set_name("ButtonServer_" + game.name)
+		button.set_meta("Name", game.name)
 		button.set_meta("IP", game.ip)
 		button.set_meta("Port", game.port)
 		$HBoxContainer/MenuGames.add_child(button)
 		button.connect("pressed",self,"_on_serverSelected", [button])
 	
 func _on_serverSelected(button):
-	print(button.get_meta("IP"))
-	print(button.get_meta("Port"))
+	MP.connect_to_server(button.get_meta("Name"), button.get_meta("IP"), button.get_meta("Port"))
+	
+func showIniMenu():
+	$HBoxContainer/MenuGames.visible = false
+	$HBoxContainer/MenuIni.visible = true
+
+func showGames():
+	$HBoxContainer/MenuGames.visible = true
+	$HBoxContainer/MenuIni.visible = false
+
+func _on_newPlayer(player):
+	print(str(player) + " has connected")

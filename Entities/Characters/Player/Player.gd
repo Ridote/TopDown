2fde1e1a28f128e162b983c0c4d7d123a49721c7
Ridote <daniridao@gmail.com>
2018-12-21 20:39:34 +0000

    Syncronising anim and rotation

--- Entities/Characters/Player/Player.gd
+++ Entities/Characters/Player/Player.gd
@@ -13,6 +13,8 @@ var ANIMATIONS = {
 
 var casting : bool = false
 puppet var slave_position : Vector2 = Vector2(0,0)
+puppet var slave_rotation : float = 0.0
+puppet var slave_anim : String = ""
 
 func _ready():
 	add_to_group(Constants.G_PLAYER)
@@ -25,9 +27,13 @@ func _physics_process(delta):
 		move(delta)
 		animate()
 		rset("slave_position", $body.global_position)
+		rset("slave_rotation", $body.rotation)
 	else:
 		$body.global_position = slave_position
-	
+		$body.rotation = slave_rotation
+		if(prev_anim != slave_anim):
+			$MoveAnim.play(slave_anim)
+			prev_anim = slave_anim
 func input() -> void:
 	target_vel = Vector2(0,0)
 	target_vel.y += int(Input.is_action_pressed("ui_down"))
@@ -41,13 +47,16 @@ func animate() -> void:
 		if(prev_anim != ANIMATIONS[ANIMATION_TYPE.CASTING]):
 			prev_anim = ANIMATIONS[ANIMATION_TYPE.CASTING]
 			$MoveAnim.play(prev_anim)
+			rset("slave_anim", prev_anim)
 		return
 	if(linear_vel.length_squared()) > ANIMATION_TRIGGER_SPEED:
 		$body.look_at($body.global_position + linear_vel.normalized())
 		if(prev_anim != ANIMATIONS[ANIMATION_TYPE.RUN]):
 			prev_anim = ANIMATIONS[ANIMATION_TYPE.RUN]
 			$MoveAnim.play(prev_anim)
+			rset("slave_anim", prev_anim)
 	else:
 		if(prev_anim != ANIMATIONS[ANIMATION_TYPE.IDLE]):
 			prev_anim = ANIMATIONS[ANIMATION_TYPE.IDLE]
 			$MoveAnim.play(prev_anim)
+			rset("slave_anim", prev_anim)

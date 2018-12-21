baf2bdd9865c2ae794a480e80d1936e2466fb867
Ridote <daniridao@gmail.com>
2018-12-21 19:59:01 +0000

    Pending start button to start the game

--- Entities/Autoloads/Constants.gd
+++ Entities/Autoloads/Constants.gd
@@ -1,5 +1,8 @@
+#warnings-disable
 extends Node
 
 # GROUPS
 const G_CHARACTER = "Character"
 const G_PLAYER = "Player"
+
+var PLAYER_NICKNAME : String = "Placeholder"
--- Entities/Interface/StartScreen.gd
+++ Entities/Interface/StartScreen.gd
@@ -3,12 +3,15 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if(MP.connect("broker_list", self, "_on_retrieveFoundGames")):
-		print("ERROR WHEN CONNECTING SIGNALS")
+		pass
 	if(MP.connect("new_player", self, "_on_newPlayer")):
 		pass
-
+	if($HBoxContainer/GUITemplate/NicknameForm/VBoxContainer/HBoxContainer/Submit.connect("pressed", self, "_on_SubmitNickname")):
+		pass
+	if($HBoxContainer/GUITemplate/NicknameForm/VBoxContainer/HBoxContainer/Exit.connect("pressed", self, "_on_Exit")):
+		pass
 func _on_NewGames_pressed():
-	MP.create_server("Rido")
+	MP.create_server(Constants.PLAYER_NICKNAME)
 
 func _on_FindGames_pressed():
 	print("Find Games")
@@ -31,6 +34,13 @@ func _on_retrieveFoundGames(games):
 func _on_serverSelected(button):
 	MP.connect_to_server(button.get_meta("Name"), button.get_meta("IP"), button.get_meta("Port"))
 	
+func _on_SubmitNickname():
+	Constants.PLAYER_NICKNAME = $HBoxContainer/GUITemplate/NicknameForm/VBoxContainer/Nickname.text
+	$HBoxContainer/GUITemplate/NicknameForm.visible = false
+	showIniMenu()
+	
+func _on_Exit():
+	get_tree().quit()
 	
 func showIniMenu():
 	$HBoxContainer/GUITemplate/MenuGames.visible = false
@@ -44,6 +54,6 @@ func _on_newPlayer(player):
 	print(str(player) + " has connected")
 	var players = MP.connected_players()
 	var playersText = $HBoxContainer/GUITemplate/MenuGames/Lobby/PlayersOnLobby
-	playersText = ""
+	playersText.text = ""
 	for player in players:
 		playersText.text += str(player) + "\n"

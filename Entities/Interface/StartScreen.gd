bce3d747d72b6138f65eb55cc2d42a011a7c1824
Ridote <daniridao@gmail.com>
2018-12-21 20:25:55 +0000

    Breaking the GUI for the greater good

--- Entities/Interface/StartScreen.gd
+++ Entities/Interface/StartScreen.gd
@@ -1,4 +1,4 @@
-extends Control
+extends Node2D
 
 const playerFactory = preload("res://Entities/Characters/Player/Player.tscn")
 
@@ -9,11 +9,11 @@ func _ready():
 		pass
 	if(MP.connect("new_player", self, "_on_newPlayer")):
 		pass
-	if($HBoxContainer/GUITemplate/NicknameForm/VBoxContainer/HBoxContainer/Submit.connect("pressed", self, "_on_SubmitNickname")):
+	if($StartScreen/HBoxContainer/GUITemplate/NicknameForm/VBoxContainer/HBoxContainer/Submit.connect("pressed", self, "_on_SubmitNickname")):
 		pass
-	if($HBoxContainer/GUITemplate/NicknameForm/VBoxContainer/HBoxContainer/Exit.connect("pressed", self, "_on_Exit")):
+	if($StartScreen/HBoxContainer/GUITemplate/NicknameForm/VBoxContainer/HBoxContainer/Exit.connect("pressed", self, "_on_Exit")):
 		pass
-	if($HBoxContainer/GUITemplate/MenuGames/Lobby/StartGame.connect("pressed", self, "_on_StartGame")):
+	if($StartScreen/HBoxContainer/GUITemplate/MenuGames/Lobby/StartGame.connect("pressed", self, "_on_StartGame")):
 		pass
 
 func _on_NewGames_pressed():
@@ -34,36 +34,38 @@ func _on_retrieveFoundGames(games):
 		button.set_meta("Name", game.name)
 		button.set_meta("IP", game.ip)
 		button.set_meta("Port", game.port)
-		$HBoxContainer/GUITemplate/MenuGames/GamesCentered/Games.add_child(button)
+		$StartScreen/HBoxContainer/GUITemplate/MenuGames/GamesCentered/Games.add_child(button)
 		button.connect("pressed",self,"_on_serverSelected", [button])
 	
 func _on_serverSelected(button):
 	MP.connect_to_server(button.get_meta("Name"), button.get_meta("IP"), button.get_meta("Port"))
 	
 func _on_SubmitNickname():
-	Constants.PLAYER_NICKNAME = $HBoxContainer/GUITemplate/NicknameForm/VBoxContainer/Nickname.text
-	$HBoxContainer/GUITemplate/NicknameForm.visible = false
+	Constants.PLAYER_NICKNAME = $StartScreen/HBoxContainer/GUITemplate/NicknameForm/VBoxContainer/Nickname.text
+	$StartScreen/HBoxContainer/GUITemplate/NicknameForm.visible = false
 	showIniMenu()
 	
 func _on_Exit():
 	get_tree().quit()
 	
 func showIniMenu():
-	$HBoxContainer/GUITemplate/MenuGames.visible = false
-	$HBoxContainer/GUITemplate/MenuIni.visible = true
+	$StartScreen/HBoxContainer/GUITemplate/MenuGames.visible = false
+	$StartScreen/HBoxContainer/GUITemplate/MenuIni.visible = true
 
 func showGames():
-	$HBoxContainer/GUITemplate/MenuGames.visible = true
-	$HBoxContainer/GUITemplate/MenuIni.visible = false
+	$StartScreen/HBoxContainer/GUITemplate/MenuGames.visible = true
+	$StartScreen/HBoxContainer/GUITemplate/MenuIni.visible = false
 
 func _on_newPlayer(player):
 	print(str(player) + " has connected")
 	var players = MP.connected_players()
-	var playersText = $HBoxContainer/GUITemplate/MenuGames/Lobby/PlayersOnLobby
+	var playersText = $StartScreen/HBoxContainer/GUITemplate/MenuGames/Lobby/PlayersOnLobby
 	playersText.text = ""
 	for player in players:
 		playersText.text += str(player) + "\n"
 
 sync func _on_StartGame():
-	if(get_tree().change_scene("res://Entities/Maps/TestMap.tscn")):
-		print("ERROR WHEN LOADING SCENE")
+	$StartScreen.visible = false
+	$Map.spawn()
+	$Map.visible = true
+	$StartScreen.queue_free()
--- Entities/Maps/TestMap.gd
+++ Entities/Maps/TestMap.gd
@@ -1,4 +1,4 @@
 extends Node2D
 
-func _ready():
-	MP.spawn_type("Player", Constants.PLAYER_NICKNAME, "/root/Map")
+func spawn():
+	MP.spawn_type("Player", Constants.PLAYER_NICKNAME, "/root/Main/Map")
